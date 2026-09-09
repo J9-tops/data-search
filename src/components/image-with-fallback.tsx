48758650a24d81c40b025ba2f334b6b8fedc38a5
Jimmy <j9.tops@gmail.com>
2026-09-09 11:58:42 +0100

    fix: correct errors

--- src/components/image-with-fallback.tsx
+++ src/components/image-with-fallback.tsx
@@ -1,4 +1,5 @@
-import { useState, ImgHTMLAttributes, useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import type { ImgHTMLAttributes } from 'react';
 import { appendImageDimensions } from '../utils/image';
 
 interface Props extends ImgHTMLAttributes<HTMLImageElement> {
